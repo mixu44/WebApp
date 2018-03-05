31413fa94e58350a45c429098075dc8cf0cd573d
Rasmus Nøhr <Rasmus-nohr@hotmail.com>
2018-03-05 20:10:05 +0100

    updated modal

--- WebApp/wwwroot/js/site.js
+++ WebApp/wwwroot/js/site.js
@@ -1,1 +1,19 @@
-﻿// Write your JavaScript code.
+﻿// Get the modal
+var modal = document.getElementById('myModal');
+
+var btn = document.getElementById("send");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
