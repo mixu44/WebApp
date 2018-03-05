4197b2b2fe048152f1b71253e2466251c9c3231e
Rasmus Nøhr <Rasmus-nohr@hotmail.com>
2018-03-05 22:23:26 +0100

    Some small adjustments

--- WebApp/Models/Car.cs
+++ WebApp/Models/Car.cs
@@ -1,14 +1,31 @@
-﻿namespace WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Models
 {
     public class Car
     {
 
         public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Brand { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string Model { get; set; }
+
+        [Range(1, 2018)]
+        [Required]
         public int Year { get; set; }
+
+        [Range(1, 1500)]
         public int Hk { get; set; }
-        public int Price { get; set; }
+
+        [Range(1, 2000000)]
+        [DataType(DataType.Currency)]
+        [Required]
+        public decimal Price { get; set; }
 
 
 
